d959c82e7036a861537e03218852f1aad3689938
softchris <noringc@gmail.com>
2022-02-23 17:40:06 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/softchris/logger
+
+go 1.16
---
+++ log.go
@@ -0,0 +1,11 @@
+package logger
+
+import (
+	"fmt"
+)
+
+var Version string = "1.0"
+
+func Log(mess string) {
+	fmt.Println("[LOG] " + mess)
+}
